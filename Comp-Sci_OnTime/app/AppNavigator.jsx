4b6f1948442de3b168150f3edc169afd432701c7
ZAMED7 <99867659+ZAMED7@users.noreply.github.com>
2024-12-10 20:45:50 +0000

    take 4

--- Comp-Sci_OnTime/app/AppNavigator.jsx
+++ Comp-Sci_OnTime/app/AppNavigator.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
 import LoginPage from './components/LoginPage';
 import SignUpPage from './components/SignUpPage';
 import ForgotPassword from './components/ForgotPassword';
@@ -13,16 +14,18 @@ const Stack = createStackNavigator();
 
 const AppNavigator = () => {
   return (
-    <Stack.Navigator initialRouteName="Login">
-      <Stack.Screen name="Login" component={LoginPage} options={{headerShown: false}} />
-      <Stack.Screen name="SignUp" component={SignUpPage} options={{headerShown: false}} />
-      <Stack.Screen name="ForgotPassword" component={ForgotPassword} options={{headerShown: false}} />
-      <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
-      <Stack.Screen name="Homework" component={HomeworkPage} options={{ headerShown: false }} />
-      <Stack.Screen name="Calender" component={CalenderPage} />
-      <Stack.Screen name="Timetable" component={TimetablePage} />
-      <Stack.Screen name="Settings" component={Settings} />
-    </Stack.Navigator>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginPage} options={{headerShown: false}} />
+        <Stack.Screen name="SignUp" component={SignUpPage} options={{headerShown: false}} />
+        <Stack.Screen name="ForgotPassword" component={ForgotPassword} options={{headerShown: false}} />
+        <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Homework" component={HomeworkPage} options={{ headerShown: false }} />
+        <Stack.Screen name="Calender" component={CalenderPage} />
+        <Stack.Screen name="Timetable" component={TimetablePage} />
+        <Stack.Screen name="Settings" component={Settings} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
